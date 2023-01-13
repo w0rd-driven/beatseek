7063b8e31d63571c9ffe93a100da5985615190ff
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2023-01-13 03:54:38 -0500

    Artist transform
    Mix format finally caught these
    Reduced scanner to a full scan with comments and the reduced version using a capture operator.
    Add artist transform to turn our struct into the Artist's representation of attributes.

--- lib/beatseek/scanner.ex
+++ lib/beatseek/scanner.ex
@@ -1,85 +1,5 @@
 defmodule Beatseek.Scanner do
-  def search() do
-    # file_name = "/Users/jbrayton/Music/iTunes/iTunes Media/Music/Periphery/Juggernaut_ Alpha/1-01 A Black Minute.mp3"
-
-    # Beatseek.MP3Stat.parse(file_name)
-
-    directory = "/Users/jbrayton/Music/iTunes/iTunes Media/Music"
-    files = "/**/**/*.mp3"
-
-    path = Path.join(directory, files)
-
-    Path.wildcard(path)
-    |> Enum.map(fn path ->
-      # Read tags
-      case Beatseek.MP3Stat.parse(path) do
-        {:ok, tag} -> tag
-        {:error, _error} -> %{}
-      end
-    end)
-    |> Enum.uniq_by(fn tag ->
-      # Filter down to unique artist/album pairs
-      # It would be quicker to use a MapSet to stay unique
-      # ETS could work but overkill, why not benchmark it?
-      if tag != %{} do
-        tag.artist && tag.album
-      end
-    end)
-    |> Enum.sort_by(
-      fn tag ->
-        tag.artist
-      end,
-      :asc
-    )
-
-    # LiveBeats takes about 70+ seconds for everything on my machine
-    #   this is doing the extra work of determining the duration, which we don't need.
-    #   I could perform the surgery needed to strip that out.
-    # There are already issues with this though, like the year possibly being TYER or TDRC
-    # It's much easier to just use the Rust implementation, especially considering we may make that
-    #   our "collector".
-    # Elixir-native parsing isn't a terrible idea eventually, I just don't have the time as a capstone.
-  end
-
-  def better_search() do
-    directory = "/Users/jbrayton/Music/iTunes/iTunes Media/Music"
-    files = "/**/*.mp3"
-
-    path = Path.join(directory, files)
-
-    Path.wildcard(path)
-    |> Enum.uniq_by(fn file_path ->
-      # Now we filter by parent directory, from 4823 to 443
-      file_path
-      |> Path.dirname()
-
-      # |> IO.inspect(label: "Basename")
-    end)
-    |> Enum.map(fn path ->
-      # Read tags
-      case Beatseek.MP3Stat.parse(path) do
-        {:ok, tag} -> tag
-        {:error, _error} -> %{}
-      end
-    end)
-    |> Enum.uniq_by(fn tag ->
-      # Filter down to unique artist/album pairs
-      # It would be quicker to use a MapSet to stay unique
-      # ETS could work but overkill, why not benchmark it?
-      if tag != %{} do
-        tag.artist && tag.album
-      end
-    end)
-    |> Enum.sort_by(
-      fn tag ->
-        tag.artist
-      end,
-      :asc
-    )
-  end
-
-  def even_better_search() do
-    directory = "/Users/jbrayton/Music/iTunes/iTunes Media/Music"
+  def full_scan(directory \\ "/Users/jbrayton/Music/iTunes/iTunes Media/Music") do
     files = "/**/*.mp3"
 
     path = Path.join(directory, files)
@@ -115,6 +35,24 @@ defmodule Beatseek.Scanner do
     )
   end
 
-  def benchmark() do
+  def scan(directory \\ "/Users/jbrayton/Music/iTunes/iTunes Media/Music") do
+    files = "/**/*.mp3"
+    path = Path.join(directory, files)
+
+    Path.wildcard(path)
+    |> Enum.uniq_by(&Path.dirname/1)
+    |> Enum.map(fn path ->
+      case Beatseek.MP3Stat.parse(path, no_duration: true) do
+        {:ok, tag} -> tag
+        {:error, _error} -> %{}
+      end
+    end)
+    |> Enum.uniq_by(&(&1 != %{} && (&1.artist && &1.album)))
+    |> Enum.filter(&(!is_nil(&1.artist)))
+    |> Enum.map(&create_records/1)
+  end
+
+  defp create_records(id3 \\ %{}) do
+    artist = Beatseek.Transformers.ArtistTransformer.transform(id3)
   end
 end
---
+++ lib/beatseek/transformers/artist_transformer.ex
@@ -0,0 +1,20 @@
+defmodule Beatseek.Transformers.ArtistTransformer do
+  def transform(id3) do
+    %{
+      name: id3.artist,
+      path: get_path(id3.path, id3.artist)
+    }
+  end
+
+  defp get_path(path, artist) do
+    album_directory = path |> Path.dirname()
+    artist_directory = album_directory |> Path.dirname()
+    directory_name = artist_directory |> Path.basename()
+
+    if String.jaro_distance(artist, directory_name) >= 0.5 do
+      artist_directory
+    else
+      album_directory
+    end
+  end
+end
