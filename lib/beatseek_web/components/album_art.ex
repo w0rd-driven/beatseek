3b40c0445ac45ed7ccc9ed7fe5f8524900d5cb01
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2023-01-19 11:28:04 -0500

    Image fixes
    Add font usage. I think I know part of the problem, we're blanketly setting all body elements to one font and there are no h1/h2 usage like there should be traditionally. I need to analyze each page for the heading text that I'm clearly styling and use h1-6. That'll help some.
    I also chose fonts with crappy weights that really don't seem to show boldness well or there's something happening with the default css I'm not catching any more. There was the tailwind typography plugin that I haven't really worked with before.
    Turns out I'm already using Audiowide, the font I chose to tie to the logo on the header.
    Add lazy loading to images. If loading isn't supported, browsers just work as they have previously.
    Fixed a z-index issue where the sticky header would show behind our `album_art` component.

--- lib/beatseek_web/components/album_art.ex
+++ lib/beatseek_web/components/album_art.ex
@@ -9,7 +9,7 @@ defmodule BeatseekWeb.Components.AlbumArt do
   def album_art(assigns) do
     ~H"""
     <div class={if !@is_owned, do: "relative opacity-50", else: "relative"}>
-      <img src={if is_nil(@url), do: "/images/album.svg", else: @url} class={@class} />
+      <img src={if is_nil(@url), do: "/images/album.svg", else: @url} class={@class} loading="lazy" />
       <span :if={@is_owned} class="absolute bottom-0.5 right-0.5 text-supporting-teal-400">
         <Heroicons.check_badge solid class="h-6 w-6 fill-current stroke-primary-900" />
       </span>
