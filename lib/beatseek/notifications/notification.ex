7f3f803794a1f4dc7b0cbcd9a2a5c8b8ab532c90
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2023-01-19 17:30:57 -0500

    #1 #13 Add ecto.dump and refactored notification type
    I can see a need for generic notifications for when background processes complete. Will need to adjust tests for this later. I'm too lazy to run the generators again.
    
    Using `ecto.load` is somehow problematic though. I don't have the steps down right I think as I try to drop/create and then load because it wants a database but I had foreign key constraint issues afterward. I suspect I'll just do a backup and restore if I can get TablePlus to work.

--- lib/beatseek/notifications/notification.ex
+++ lib/beatseek/notifications/notification.ex
@@ -7,7 +7,7 @@ defmodule Beatseek.Notifications.Notification do
     field :icon, :string
     field :seen_at, :utc_datetime_usec
     field :subject, :string
-    field :type, Ecto.Enum, values: [:not_owned, :new_release, :upcoming_release]
+    field :type, Ecto.Enum, values: [:album_not_owned, :album_new_release, :album_upcoming_release]
     field :url, :string
     field :album_id, :id
 
@@ -18,6 +18,6 @@ defmodule Beatseek.Notifications.Notification do
   def changeset(notification, attrs) do
     notification
     |> cast(attrs, [:icon, :subject, :body, :url, :type, :seen_at])
-    |> validate_required([:subject, :type])
+    |> validate_required([:subject])
   end
 end
