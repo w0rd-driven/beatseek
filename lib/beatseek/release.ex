6dd3e13749a1f62fd7adb57a098ac651d42671be
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2023-01-20 15:46:00 -0500

    #13 Add drop to releases
    I restored over the wrong database :facepalm:. I think TablePlus works as I saw the data but perhaps it requires no database. The data wouldn't change. I also didn't look into a --force option.
    
    There can be a use case for this for releases. Taken from https://elixirforum.com/t/drop-database-with-elixir-releases/32229.

--- lib/beatseek/release.ex
+++ lib/beatseek/release.ex
@@ -18,6 +18,27 @@ defmodule Beatseek.Release do
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
+  def ecto_drop do
+    load_app()
+
+    for repo <- repos() do
+      drop_database(repo)
+    end
+  end
+
+  defp drop_database(repo) do
+    case repo.__adapter__.storage_down(repo.config) do
+      :ok ->
+        IO.puts "The database for #{inspect repo} has been dropped"
+      {:error, :already_down} ->
+        IO.puts "The database for #{inspect repo} has already been dropped"
+      {:error, term} when is_binary(term) ->
+        raise "The database for #{inspect repo} couldn't be dropped: #{term}"
+      {:error, term} ->
+        raise "The database for #{inspect repo} couldn't be dropped: #{inspect term}"
+    end
+  end
+
   defp repos do
     Application.fetch_env!(@app, :ecto_repos)
   end
