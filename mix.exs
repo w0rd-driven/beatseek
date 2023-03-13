cb17c45c689247157db1ec7de4e0007c0d2cf01e
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2023-03-13 17:17:13 -0400

    #54 Diffmerge comparisons
    Import heroicons by adding the `tailwind.config.js` changes and the directories in `priv`.
    topbar `show` method used instead of `delayedShow`.
    Improvements for runtime config.
    Use `.flash_group` component in our layout.
    Home improvements.
    Add `assets.build` mix alias.

--- mix.exs
+++ mix.exs
@@ -92,6 +92,7 @@ defmodule Beatseek.MixProject do
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
+      "assets.build": ["tailwind default", "esbuild default"],
       "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"],
       check: [
         "clean",
